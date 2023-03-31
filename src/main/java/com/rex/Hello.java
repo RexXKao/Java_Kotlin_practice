5e68c91ed7e89773b7cff5281d9ac34054272eb7
RexKao <ww10282002@gmail.com>
2023-03-31 16:01:12 +0800

    Java and Kotlin constructor

--- src/main/java/com/rex/Hello.java
+++ src/main/java/com/rex/Hello.java
@@ -3,9 +3,13 @@ package com.rex;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
-        Person p = new Person();
+        Person p = new Person("rex",66.5f,1.7f);
         p.hello();
-        int age = 19;
+        System.out.println(p.weight);
+        p.weight = 66.5f;
+        p.height = 1.7f;
+        System.out.println(p.bmi());
+        /*int age = 19;
         Integer age2 = 19;
         char c ='i';
         Character c2 = 'A';
@@ -14,6 +18,6 @@ public class Hello {
         float height = 1.7f;
         boolean adult = true;
         boolean enroll = false;
-        String name = "Rex";
+        String name = "Rex";*/
     }
 }
--- src/main/java/com/rex/Hello.kt
+++ src/main/java/com/rex/Hello.kt
@@ -3,16 +3,26 @@ package com.rex
 fun main(args: Array<String>) {
 //    println("Hello kotlin")
 //    Human().hello()
-    val h = Human()
-    h.hello()
-    val age : Int = 19;
+    val h = Human(66.5f, 1.7f)
+    println(h.bmi())
+    /*val age = 19;
     age = 20;
     var weight = 66.5;
     var name : String
-    name = "Rex";
+    name = "Rex";*/
 }
 
-class Human {
+class Human(var weight: Float, var height: Float, var name: String = "") {
+    init {
+        println("test $weight")
+    }
+
+//    constructor(name:String, weight: Float, height: Float) :this(weight, height)
+
+    fun bmi(): Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
     fun hello() {
         println("Hello kotlin")
     }
