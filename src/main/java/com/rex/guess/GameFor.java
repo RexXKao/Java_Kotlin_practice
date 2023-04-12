f04b010002927570d37d71d3bd65e2c3332f9f75
RexKao <ww10282002@gmail.com>
2023-04-12 18:57:11 +0800

    Java and Kotlin guess number 'for' game

---
+++ src/main/java/com/rex/guess/GameFor.java
@@ -0,0 +1,34 @@
+package com.rex.guess;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameFor {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        int secret = new Random().nextInt(10) + 1;
+        System.out.println(secret);
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Please enter a number(" + i + "/4): ");
+            int number = scanner.nextInt();
+            System.out.println("第" + i + "次:" + number);
+            if (number < secret) {
+                System.out.println("higher");
+            } else if (number > secret) {
+                System.out.println("lower");
+            } else {
+                System.out.println("Great, the number is " + number);
+                break;
+            }
+        }
+        System.out.println("結束");
+    }
+}
